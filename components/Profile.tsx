b3615443da3971b3482e6c1328556939e83a1ec9
Renato Santos <renato.senstec@gmail.com>
2023-01-30 16:40:54 -0300

    test img import

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import profileImg from "@/public/img/profileImg.png"
+import profileImg from "../public/img/profileImg.png"
 
 import config from "@/portfolioConfig.json"
 
