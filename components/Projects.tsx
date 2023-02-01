a913f3953ed630de8097e39b06dfd2cc8d61d422
Renato Santos <renato.senstec@gmail.com>
2023-02-01 11:25:38 -0300

    projects page finished

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,27 +1,25 @@
 import { portfolio } from "@/portfolio"
-import Image from "next/image"
-import { NavIcons } from "./NavIcons"
-import { v4 as uuidv4 } from "uuid"
 import { Card } from "./Card"
 
 interface IProjectsProps {
-  length: number
+  start: number
+  end?: number
 }
 
-export function Projects({ length }: IProjectsProps) {
+export function Projects({ start, end }: IProjectsProps) {
   return (
     <>
       <section className="flex flex-col items-center justify-center mt-6 text-white ">
         <h2 className="mb-4 text-xl text-center text-white md:text-4xl text-bold">
           Projects
         </h2>
-        <div className="flex flex-col gap-4 md:flex md:flex-row md:gap-[73px] md:mt-[50px] ">
-          {portfolio.projects.slice(0, length).map((project) => (
+        <div className="flex flex-col justify-center items-center flex-wrap gap-4 md:flex md:flex-row md:gap-[73px] md:mt-[50px] ">
+          {portfolio.projects.slice(start, end).map((project) => (
             <Card key={project.name} {...project} />
           ))}
         </div>
+        <div className=" mt-[32px] md:m-[73px] self-center rounded-full shadow-sm w-[300px] md:w-[675px] shadow-brand bg-brand h-[5px]"></div>
       </section>
-      <div className=" mt-[32px] md:m-[73px] rounded-full shadow-sm w-[300px] md:w-[675px] shadow-brand bg-brand h-[5px]"></div>
     </>
   )
 }
